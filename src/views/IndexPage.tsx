b6f5e7eda0521eabb01c0941d6b2eb95f1839df6
Mileto <milenkotomasevic41@gmail.com>
2024-08-21 22:54:10 -0400

    dev: Removed header of pages

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,9 +1,7 @@
-import Header from "../components/Header";
-
 export default function IndexPage() {
-  return (
-    <>
-        <Header/>
-    </>
-  )
+	return (
+		<>
+			<h1>Inicio</h1>
+		</>
+	);
 }
