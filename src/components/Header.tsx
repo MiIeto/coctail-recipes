1603690890e37be783fcd6d008d30c705e8347b9
Mileto <milenkotomasevic41@gmail.com>
2024-08-21 22:57:37 -0400

    dev: Added Link component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { Link, NavLink } from 'react-router-dom';
+
 export default function Header() {
 	return (
 		<header className='bg-slate-800'>
@@ -10,7 +12,20 @@ export default function Header() {
 							alt='logotipo'
 						/>
 					</div>
-					<nav></nav>
+					<nav className='flex gap-4'>
+						<Link
+							to='/'
+							className='text-white uppercase font-bold'
+						>
+							Inicio
+						</Link>
+						<Link
+							to='/favoritos'
+							className='text-white uppercase font-bold'
+						>
+							Favoritos
+						</Link>
+					</nav>
 				</div>
 			</div>
 		</header>
