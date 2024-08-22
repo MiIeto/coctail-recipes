608cd32d83080902c839a63a04f52640888d8e0c
Mileto <milenkotomasevic41@gmail.com>
2024-08-21 23:50:28 -0400

    dev: Added new type and fetch categories set function

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,16 +1,18 @@
-import { StateCreator } from "zustand";
-import { getCategories } from "../services/RecipeService";
-
-type Category = {}
+import { StateCreator } from 'zustand';
+import { getCategories } from '../services/RecipeService';
+import type { Categories } from '../types';
 
 export type RecipeSliceType = {
-    categories: Category[];
-    fetchCategories: () => Promise<void>;
-}
+	categories: Categories;
+	fetchCategories: () => Promise<void>;
+};
 
-export const createRecipeSlice : StateCreator<RecipeSliceType> = () => ({
-	categories: [],
-    fetchCategories: async () => {
-        getCategories()
-    },
+export const createRecipeSlice: StateCreator<RecipeSliceType> = (set) => ({
+	categories: {
+		drinks: [],
+	},
+	fetchCategories: async () => {
+		const categories = await getCategories();
+		set({ categories });
+	},
 });
