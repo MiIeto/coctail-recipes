10c15d75642db75fd78ce1c2bdbb8fe84e5187d6
Mileto <milenkotomasevic41@gmail.com>
2024-08-22 00:08:24 -0400

    dev: Fixed response from url

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { CategoriesAPIResponseSchema } from '../utils/recipes-schema';
 
 export async function getCategories() {
-	const url = 'www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+	const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
 	const { data } = await axios(url);
 	const result = CategoriesAPIResponseSchema.safeParse(data);
 	if (result.success) {
