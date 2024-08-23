e2fc66ec1f69e365f54e5deb8d46ce1d0e98b670
Mileto <milenkotomasevic41@gmail.com>
2024-08-23 02:13:24 -0400

    dev: Added onclick function to set recipe id

--- src/components/DrinkCard.tsx
+++ src/components/DrinkCard.tsx
@@ -1,16 +1,18 @@
 import type { Drink } from '../types';
+import { useAppStore } from '../stores/useAppStore';
 
 type DrinkCardProps = {
 	drink: Drink;
 };
 export default function DrinkCard({ drink }: DrinkCardProps) {
+	const selectRecipe = useAppStore((state) => state.selectRecipe);
 	return (
 		<div className='border shadow-lg'>
 			<div className='overflow-hidden'>
 				<img
 					src={drink.strDrinkThumb}
 					alt={`Imagen de ${drink.strDrink}`}
-                    className='hover:scale-125 transition-transform hover:rotate-2 duration-300'
+					className='hover:scale-125 transition-transform hover:rotate-2 duration-300'
 				/>
 			</div>
 			<div className='p-5'>
@@ -20,7 +22,10 @@ export default function DrinkCard({ drink }: DrinkCardProps) {
 				<button
 					type='button'
 					className='bg-orange-400 hover:bg-orange-500 mt-5 w-full p-4 font-bold text-white text-lg'
-				>Ver Receta</button>
+					onClick={() => selectRecipe(drink.idDrink)}
+				>
+					Ver Receta
+				</button>
 			</div>
 		</div>
 	);
