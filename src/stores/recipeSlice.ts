986f0e90a0c4264a320419c8ecccdf2f3ebe0072
Mileto <milenkotomasevic41@gmail.com>
2024-08-23 01:43:31 -0400

    dev: Added getRecipes to slice

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,5 +1,5 @@
 import { StateCreator } from 'zustand';
-import { getCategories } from '../services/RecipeService';
+import { getCategories, getRecipes } from '../services/RecipeService';
 import type { Categories, SearchFilter } from '../types';
 
 export type RecipeSliceType = {
@@ -17,6 +17,6 @@ export const createRecipeSlice: StateCreator<RecipeSliceType> = (set) => ({
 		set({ categories });
 	},
 	searchRecipes: async (filters) => {
-		console.log(filters);
+		await getRecipes(filters);
 	},
 });
